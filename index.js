a6b359305b394271ae026ff054214f37a70f8fd0
Eddy Oliveira Mesquita <eddy_eom@hotmail.com>
2024-09-08 14:34:32 -0300

    vercel pgsql connection

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    withCredentials: true,
 }));
 
 app.use('/auth', authRoutes);
